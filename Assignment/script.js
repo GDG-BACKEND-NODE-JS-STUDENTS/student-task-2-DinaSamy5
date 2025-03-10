fdfa3a8bb264415ea4f27bd46e5764884ce9aaa8
DinaSamy5 <https://github.com/DinaSamy5>
2025-03-11 00:38:33 +0200

    done

--- Assignment/script.js
+++ Assignment/script.js
@@ -1,1 +1,13 @@
 // Write the codes in here
+let name = "dina";
+const age = 20;
+var test = 19;
+
+let sum = age + test;
+let test_age = age > 18;
+
+let s = age.toString();
+
+console.log("age",age);
+console.log("string",s);
+console.log("test age",test_age);
